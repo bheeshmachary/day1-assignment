48ed7844347ce35a8054ee4d587e960daf215b9b
bheeshmachary <118670478+bheeshmachary@users.noreply.github.com>
2022-11-27 14:41:36 +0530

    Add files via upload

---
+++ helloworld.java
@@ -0,0 +1,11 @@
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hello\tprogram");
+System.out.println("demo 1/n/n/n");
+System.out.println("demo 2");
+	}
+
+}
